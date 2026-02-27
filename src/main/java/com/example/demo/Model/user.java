20ec2edadf6bb9fd919d1538ee71b739f6208e6c
Ahsen Ali <116498245+Ahsen2603@users.noreply.github.com>
2026-02-27 23:06:21 +0000

    Add user model with validation constraints and update pom.xml for validation dependency

---
+++ src/main/java/com/example/demo/Model/user.java
@@ -0,0 +1,52 @@
+package com.example.demo.Model;
+
+import java.io.Serializable;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "users")
+
+public class user implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    long id;
+
+    @NotBlank(message = "Name is mandatory")
+    String name;
+    @NotBlank(message = "Email is mandatory")       
+    String email;
+    @NotBlank(message = "Password is mandatory")
+    String password;
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
